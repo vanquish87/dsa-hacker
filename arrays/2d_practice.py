e1efa83452024d19f149935735f813d308c40f13
Jasmeet Singh <mailtojasmeet@gmail.com>
2022-11-13 10:03:25 +0530

    added array with more hackerrank practice n revision of topics

---
+++ arrays/2d_practice.py
@@ -0,0 +1,60 @@
+#   Created by Elshad Karimov on 05/04/2020.
+#   Copyright © 2020 AppMillers. All rights reserved.
+
+import numpy as np
+
+oneDAarray = np.array([1 ,4, 5, 8, 5, 57])
+print(oneDAarray)
+
+twoDArray = np.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9] ])
+print(twoDArray)
+
+print('----')
+newTwoDArray = np.insert(twoDArray, 1, [[1,2,3,4]], axis=0)
+print(newTwoDArray)
+
+print('----')
+print(len(twoDArray))
+
+
+print('----')
+newTwoDArray = np.append(twoDArray, [[1,2,3,4]], axis=0)
+print(newTwoDArray)
+print(len(newTwoDArray))
+print(len(newTwoDArray[0]))
+
+
+def accessElements(array, rowIndex, colIndex):
+    if rowIndex >= len(array) and colIndex >= len(array[0]):
+        print('Incorrect Index')
+    else:
+        print(array[rowIndex][colIndex])
+
+print('----')
+accessElements(newTwoDArray, 1, 3)
+
+def traverseTDArray(array):
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            print(array[i][j], end=" ")
+        print(' ')
+
+
+print('----')
+traverseTDArray(twoDArray)
+
+
+def searchTDArray(array, value):
+    for i in range(len(array)):
+        for j in range(len(array[0])):
+            if array[i][j] == value:
+                return 'The value is located index '+str(i)+" "+str(j)
+    return 'The element no found'
+
+
+print('----')
+print(searchTDArray(twoDArray, 444))
+
+newTDArray = np.delete(twoDArray, 1, axis=1)
+print('----')
+print(newTDArray)
